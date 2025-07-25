e7b8178e76644854f30dfa6c500f6cdca35b0924
GrasshoppersResources <deinvadda69@web.de>
2025-07-25 21:36:17 +0200

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,1 +1,24 @@
+//OPENCV DOWNLOAD
+// https://github.com/thommyho/Cpp-OpenCV-Windows-PreBuilts
+// https://opencv.org/releases/
+
+#include <filesystem>
+#include <vector>
+
+#include "Processor.h"
+
+int main() 
+{
+    //iterate over jpg files in the input folder and create records from them
+    std::filesystem::path inputDir = std::filesystem::current_path() / "input";
+    for (const auto& entry : std::filesystem::directory_iterator(inputDir)) {
+        if (entry.path().extension() == ".jpp" || entry.path().extension() == ".JPG")
+        {
+            std::string name = entry.path().filename().stem().string();
+            Processor process(name);
+        }
+    }
+    return 0;
+}
+
 
