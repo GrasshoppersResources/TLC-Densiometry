c9d203f8e7886e80076ae00929ff5c828e583aa5
GrasshoppersResources <deinvadda69@web.de>
2025-07-26 11:00:29 +0200

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,7 +12,7 @@ int main()
     //iterate over jpg files in the input folder and create records from them
     std::filesystem::path inputDir = std::filesystem::current_path() / "input";
     for (const auto& entry : std::filesystem::directory_iterator(inputDir)) {
-        if (entry.path().extension() == ".jpp" || entry.path().extension() == ".JPG")
+        if (entry.path().extension() == ".jpg" || entry.path().extension() == ".JPG")
         {
             std::string name = entry.path().filename().stem().string();
             Processor process(name);
@@ -20,5 +20,4 @@ int main()
     }
     return 0;
 }
-
 
